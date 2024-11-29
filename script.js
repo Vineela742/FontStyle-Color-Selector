769e5306e3e8851286b94e8656037793ee755724
Vineela742 <161962136+Vineela742@users.noreply.github.com>
2024-11-29 16:54:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeColor(color) {
+    document.getElementById('text').style.color = color;
+}
+
+
+function changeTextBackground(bg) {
+    document.getElementById('text').style.backgroundColor = bg;
+}
+
+
+function changeFont(font) {
+    document.getElementById('text').style.fontFamily = font;
+}
+
